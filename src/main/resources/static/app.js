60d1578e90ba683ab674d7f8339c3a6a929e3c8a
Jon Jackson <ocskier@gmail.com>
2021-06-17 12:12:14 -0400

    feat: adds toast on update and style

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,11 +1,18 @@
 async function deleteById(e,id) {
     console.log(id);
-    const res = await fetch('/api/customers/'+ id,{
-        method: 'DELETE'
-    });
-    const data = await res.json();
-    console.log(data);
-    if (data) location.reload();
+    try {
+        const res = await fetch('/api/customers/'+ id,{
+            method: 'DELETE'
+        });
+        const data = await res.json();
+        console.log(data);
+        if (data) location.reload();
+    } catch (err) {
+        document.querySelector('#error-toast').style.display = 'block';
+        window.setTimeout(function(){
+          location.reload();
+        }, 2200)
+    }
 }
 
 async function updateById(e,id) {
@@ -16,15 +23,27 @@ async function updateById(e,id) {
         address: document.querySelector('input[name="address"]').value,
         date: document.querySelector('input[type="date"]').value,
     }
-    const res = await fetch('/api/customers/'+ id,{
-        method: 'PUT',
-        headers: {
-        "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData)
-    });
-    const data = await res.json();
-    console.log('Records updated: ', data);
-    if (data) location.replace('/hello');
+    try {
+        const res = await fetch('/api/customers/'+ id,{
+            method: 'PUT',
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData)
+        });
+        const data = await res.json();
+        if (data) {
+            console.log('Records updated: ', data);
+            document.querySelector('#update-toast').style.display = 'block';
+            window.setTimeout(function(){
+              location.replace('/hello');
+            }, 2200)
+        }
+    } catch (err) {
+        document.querySelector('#error-toast').style.display = 'block';
+        window.setTimeout(function(){
+            location.reload();
+        }, 2200)
+    }
 }
 
